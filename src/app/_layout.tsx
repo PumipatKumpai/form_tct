47a467572720d6071af379ddac6f67d604cb8a50
PumipatKumpai <fraem045620@hotmail.com>
2026-07-22 23:26:34 +0700

    Complete mobile app

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,44 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#4D7CFE",
+        },
+        headerTintColor: "#FFFFFF",
+        headerTitleAlign: "center",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+        contentStyle: {
+          backgroundColor: "#EEF5FF",
+        },
+        animation: "slide_from_right",
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "App Menu",
+          headerBackVisible: false,
+        }}
+      />
+
+      <Stack.Screen
+        name="arith"
+        options={{
+          title: "Arithmetic Calculator",
+        }}
+      />
+
+      <Stack.Screen
+        name="form"
+        options={{
+          title: "Registration Form",
+        }}
+      />
+    </Stack>
+  );
 }
